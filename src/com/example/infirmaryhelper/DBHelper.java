7ca09704c6e5eec51f8d302158d0c39fc3c1083b
ericli0625 <ericli0625@gmail.com>
2013-01-05 22:19:49 +0800

    setting city array
    setting all city array

--- src/com/example/infirmaryhelper/DBHelper.java
+++ src/com/example/infirmaryhelper/DBHelper.java
@@ -7,9 +7,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-	private final static String DATABASE_NAME = "todo.db";
+	private final static String DATABASE_NAME = "infirmaryhelper.db";
 	private final static int DATABASE_VERSION = 1;
-	private final static String TABLE_NAME = "taoyuan";
+	private final static String TABLE_NAME = "keelung_city";
 	private final static String FIELD_Id = "_id";
 	public final static String FIELD_Name = "name";
 	private final static String FIELD_Cities = "cities";
@@ -17,6 +17,14 @@ public class DBHelper extends SQLiteOpenHelper {
 	private final static String FIELD_Category = "category";
 	public final static String FIELD_Address = "address";
 	private final static String FIELD_Telephone = "telephone";
+
+	private String xinbei_city = "xinbei_city";
+	private String taoyuan_county = "taoyuan_county";
+	private String nantou_county = "nantou_county";
+	private String miaoli_county = "miaoli_county";
+	private String keelung_city = "keelung_city";
+	private String hsinchu_county = "hsinchu_county";
+	private String hsinchu_city = "hsinchu_city";
 
 	public DBHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -54,28 +62,13 @@ public class DBHelper extends SQLiteOpenHelper {
 	 * rawQuery()方法的第一个参数为select语句；第二个参数为select语句中占位符参数的值，如果select语句没有使用占位符，
 	 * 该参数可以设置为null。带占位符参数的select语句使用例子如下：
 	 */
-	public Cursor getData(String citesTag, String cityTag, String categoryTag) {
-		SQLiteDatabase db = this.getReadableDatabase();
-
-		String sql = "select * from " + TABLE_NAME + " where " + FIELD_Cities
-				+ " like ? and " + FIELD_City + " like ? and " + FIELD_Category
-				+ " like ?";
-
-		Cursor cursor = db.rawQuery(sql, new String[] { citesTag, cityTag,
-				categoryTag });
-
-		// 注意：不寫會出錯
-		if (cursor != null) {
-			cursor.moveToFirst(); // 將指標移到第一筆資料
-		}
-		return cursor;
-	}
 
 	public Cursor getData(String cityTag, String categoryTag) {
 		SQLiteDatabase db = this.getReadableDatabase();
 
 		String sql = "select * from " + TABLE_NAME + " where " + FIELD_City
-				+ " like '"+cityTag+"' and "+FIELD_Category+" like '"+categoryTag+"' ";
+				+ " like '" + cityTag + "' and " + FIELD_Category + " like '"
+				+ categoryTag + "' ";
 
 		Cursor cursor = db.rawQuery(sql, null);
 
--- src/com/example/infirmaryhelper/MainActivity.java
+++ src/com/example/infirmaryhelper/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteCursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
@@ -20,32 +21,11 @@ import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
-	private String[] cities = new String[] { "基隆市", "台北市", "新北市", "桃園縣", "新竹市",
-			"新竹縣", "苗栗縣", "台中市", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "台南市",
-			"高雄市", "屏東縣", "宜蘭縣", "花蓮縣", "台東縣", "澎湖縣", "金門縣", "連江縣" };// 載入第一下拉選單
-
-	private String[] city = new String[] { "仁愛區", "中正區", "信義區", "中山區", "安樂區", "暖暖區", "七堵區" };// 起始畫面時預先載入第二下拉選單
-
-	private String[] category = new String[] { "中醫", "牙科", "小兒科", "婦產科", "骨科",
-			"整形外科", "泌尿科", "皮膚科", "耳鼻喉科", "復健科" };// 起始畫面時預先載入第三下拉選單
-
-	// 第一下拉選取後載入第二下拉選單
-	private String[][] type2 = new String[][] {
-			{ "仁愛區", "中正區", "信義區", "中山區", "安樂區", "暖暖區", "七堵區" },
-			{ "松山區", "大安區", "大同區", "中山區", "內湖區", "南港區", "士林區", "北投區", "信義區",
-					"中正區", "萬華區", "文山區" },
-			{ "板橋區", "三重區", "永和區", "中和區", "新莊區", "新店區", "土城區", "蘆洲區", "汐止區",
-					"樹林區", "鶯歌區", "三峽區", "淡水區", "瑞芳區", "五股區", "泰山區", "林口區",
-					"深坑區", "石碇區", "坪林區", "三芝區", "石門區", "八里區", "平溪區", "雙溪區",
-					"貢寮區", "金山區", "萬里區", "烏來區" },
-			{ "桃園市", "龜山鄉", "八德市", "蘆竹鄉", "大園鄉", "大溪鎮", "復興鄉", "中壢市", "平鎮市",
-					"楊梅鎮", "龍潭鄉", "新屋鄉", "觀音鄉" } };
-
 	private Spinner spinner, spinner2, spinner3;
-	private ArrayAdapter<String> adapter;
+	private ArrayAdapter<CharSequence> adapter;
 	private Context context;
 	private String str1, str2, str3;
-	
+
 	private ListView myListView;
 	private Cursor myCursor;
 	private int _id;
@@ -68,10 +48,10 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main , menu);
+		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -82,14 +62,48 @@ public class MainActivity extends Activity {
 			openOptionsDialogAbout();
 			break;
 		case R.id.item2:
+			openOptionsDialogEmail();
+			break;
+		case R.id.item3:
 			openOptionsDialogExit();
 			break;
-
 		default:
 			break;
 		}
-		
+
 		return true;
+	}
+
+	private void openOptionsDialogEmail() {
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.app_email)
+				.setMessage(R.string.app_email_msg)
+				.setNegativeButton(R.string.str_no_mail,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// TODO Auto-generated method stub
+
+							}
+						})
+				.setPositiveButton(R.string.str_ok_mail,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// TODO Auto-generated method stub
+								Uri uri = Uri
+										.parse("mailto:ericli0625@gmail.com");
+								Intent it = new Intent(Intent.ACTION_SENDTO,
+										uri);
+								startActivity(it);
+
+							}
+						}).show();
+
 	}
 
 	private void openOptionsDialogAbout() {
@@ -135,7 +149,7 @@ public class MainActivity extends Activity {
 						}).show();
 
 	}
-	
+
 	private void ShowListView() {
 
 		SimpleCursorAdapter adapterDB = new SimpleCursorAdapter(context,
@@ -207,15 +221,15 @@ public class MainActivity extends Activity {
 	private void processSpinner() {
 		context = this;
 
-		spinner = CreateSpinner(spinner, R.id.spinner1, cities);
-		spinner2 = CreateSpinner(spinner2, R.id.spinner2, city);
-		spinner3 = CreateSpinner(spinner3, R.id.spinner3, category);
+		spinner = CreateSpinner(spinner, R.id.spinner1, R.array.cities_init);
+		spinner2 = CreateSpinner(spinner2, R.id.spinner2, R.array.city_init);
+		spinner3 = CreateSpinner(spinner3, R.id.spinner3, R.array.category_init);
 	}
 
-	private Spinner CreateSpinner(Spinner spinner, int value, String[] array) {
+	private Spinner CreateSpinner(Spinner spinner, int value, int array) {
 		// 将可选内容与ArrayAdapter连接起来
-		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, array);
+		adapter = ArrayAdapter.createFromResource(this, array,
+				android.R.layout.simple_spinner_item);
 
 		// 设置下拉列表的风格
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -248,11 +262,8 @@ public class MainActivity extends Activity {
 				long id) {
 			// 讀取第一個下拉選單是選擇第幾個
 			pos = spinner.getSelectedItemPosition();
-			// 重新產生新的Adapter，用的是二維陣列type2[pos]
-			adapter = new ArrayAdapter<String>(context,
-					android.R.layout.simple_spinner_item, type2[pos]);
 			// 載入第二個下拉選單Spinner
-			spinner2.setAdapter(adapter);
+			spinner2.setAdapter(choeseCities(pos));
 			str1 = parent.getSelectedItem().toString();
 			myCursor = DH.getData(str2, str3);
 			ShowListView();
@@ -293,5 +304,85 @@ public class MainActivity extends Activity {
 
 		}
 	};
+
+	private ArrayAdapter<CharSequence> choeseCities(int pos) {
+
+		int value = 0;
+		
+		switch (pos) {
+		case 0:
+			value = R.array.city_init;
+			break;
+		case 1:
+			value = R.array.taipei_city;
+			break;
+		case 2:
+			value = R.array.xinbei_city;
+			break;
+		case 3:
+			value = R.array.taoyuan_country;
+			break;
+		case 4:
+			value = R.array.hsinchu_city;
+			break;
+		case 5:
+			value = R.array.hsinchu_country;
+			break;
+		case 6:
+			value = R.array.miaoli_country;
+			break;
+		case 7:
+			value = R.array.taichung_city;
+			break;
+		case 8:
+			value = R.array.changhua_county;
+			break;
+		case 9:
+			value = R.array.nantou_county;
+			break;
+		case 10:
+			value = R.array.yunlin_county;
+			break;
+		case 11:
+			value = R.array.chiayi_city;
+			break;
+		case 12:
+			value = R.array.chiayi_county;
+			break;
+		case 13:
+			value = R.array.tainan_city;
+			break;
+		case 14:
+			value = R.array.Kaohsiung_city;
+			break;
+		case 15:
+			value = R.array.pingtung_county;
+			break;
+		case 16:
+			value = R.array.yilan_county;
+			break;
+		case 17:
+			value = R.array.hualien_county;
+			break;
+		case 18:
+			value = R.array.taitung_county;
+			break;
+		case 19:
+			value = R.array.penghu_county;
+			break;
+		case 20:
+			value = R.array.kinmen_county;
+			break;
+		case 21:
+			value = R.array.lianjiang_county;
+			break;
+		default:
+			break;
+		}
+
+		adapter = ArrayAdapter.createFromResource(this, value,android.R.layout.simple_spinner_item);
+
+		return adapter;
+	}
 
 }
